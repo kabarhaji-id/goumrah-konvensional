7be588ca9764e827fdd6cf504b18dcf574d150df
gipeey <hadiyusufalghifari@gmail.com>
2024-11-20 15:48:47 +0700

    optimizing for build

--- svgr.d.ts
+++ svgr.d.ts
@@ -1,11 +1,7 @@
 declare module "*.svg" {
-  import { FC, SVGProps } from "react";
-  const content: FC<SVGProps<SVGElement>>;
-  export default content;
-}
-
-declare module "*.svg?url" {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const content: any;
-  export default content;
+  import * as React from "react";
+  const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >;
+  export default ReactComponent;
 }

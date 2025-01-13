fc66850a29c08fd4e37e130abcfc37592563ac31
ItsMalma <itsmalma28@gmail.com>
2025-01-13 19:04:02 +0700

    refactor: turn navbar into single component

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,52 +1,27 @@
 "use client";
 
-import { usePathname, useRouter } from "next/navigation";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 import React from "react";
 
 import HomeIcon from "@/assets/icons/home.svg";
-import CustomerServiceIcon from "@/assets/icons/customer-service.svg";
-import KaabaIcon from "@/assets/icons/kaaba.svg";
-
-const NavbarItem = ({
-  icon: Icon,
-  text,
-  href,
-}: {
-  icon: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
-  text: string;
-  href: string;
-}) => {
-  const router = useRouter();
-  const pathname = usePathname();
-
-  const handleClick = React.useCallback(() => {
-    router.push(href);
-  }, [router, href]);
-
-  return (
-    <button
-      data-active={pathname === href ? "true" : "false"}
-      className="group flex flex-col items-center gap-1 text-foreground/30 data-[active=true]:text-primary"
-      onClick={handleClick}
-    >
-      <Icon className="size-7" />
-      <p className="whitespace-nowrap text-xs font-light tracking-normal group-data-[active=true]:font-bold">
-        {text}
-      </p>
-    </button>
-  );
-};
 
 const Navbar = () => {
+  const pathname = usePathname();
+
   return (
-    <nav className="sticky bottom-0 flex items-start justify-center gap-20 rounded-t-2xl bg-white px-12 py-4">
-      <NavbarItem icon={HomeIcon} text="Beranda" href="/" />
-      <NavbarItem
-        icon={CustomerServiceIcon}
-        text="Tanya Isma"
-        href="/tanya-isma"
-      />
-      <NavbarItem icon={KaabaIcon} text="Paket Umrah" href="/paket-umrah" />
+    <nav className="sticky bottom-0 z-50 flex items-start justify-center gap-20 rounded-t-2xl bg-white px-12 py-4">
+      <Link
+        href="/"
+        data-active={pathname == "/"}
+        className="group flex flex-col items-center gap-1 text-foreground/30 data-[active=true]:text-primary"
+      >
+        <HomeIcon className="size-7" />
+        <p className="whitespace-nowrap text-xs font-light tracking-normal group-data-[active=true]:font-bold">
+          Beranda
+        </p>
+      </Link>
     </nav>
   );
 };

50f031263ed4c791b33af2b935f008d303e20696
Rahman <cybertank378@gmail.com>
2025-02-03 13:23:13 +0700

    Update Google Analytics config and workspace settings
    Removed redundant `page_path` parameter in GA config for streamlined tracking logic. Adjusted IDE workspace settings to switch recent branch from `development` to `feat/google-tag-mnager`.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,9 +33,7 @@ export default function RootLayout({
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
-                page_path: window.location.pathname,
-              });
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
             `,
         }}
       />

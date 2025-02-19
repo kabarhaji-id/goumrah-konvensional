fdc62eb86ca5db6221ca0f4b5d6edcea19389dcf
Rahman <cybertank378@gmail.com>
2025-02-19 13:35:14 +0700

    fix Google AnalyticGoogle Tag And Meta Pixel Is Not Recorded

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,18 +1,7 @@
 import React from "react";
-import GoogleAnalytics from "@/components/analytic/google-analytics";
-import TagManager from "@/components/analytic/tag-manager";
-import FacebookPixel from "@/components/analytic/meta-pixel";
 
 const Header = () => {
-  const nonce = typeof window !== "undefined"
-    ? document.querySelector('meta[name="X-Nonce"]')?.getAttribute('content') || ''
-    : '';
   return <>
-    <meta name="X-Nonce" content={nonce} />
-
-    <GoogleAnalytics nonce={nonce} />
-    <TagManager nonce={nonce} />
-    <FacebookPixel nonce={nonce} />
   </>;
 };
 
